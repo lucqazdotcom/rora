20916a6bae746bbbee585b15ee42e96350fcbb72
SobczakL <36972429+SobczakL@users.noreply.github.com>
2026-07-27 22:35:47 -0400

    chore: edit on local url

--- client/src/services/config.js
+++ client/src/services/config.js
@@ -1,3 +1,4 @@
-export const serverURL = "https://rora-server.vercel.app";
+// export const serverURL = "https://rora-server.vercel.app";
+export const serverURL = "http://localhost:8080";
 // export const serverURL = "http://localhost:8080";
 export const apiURL = "https://external.transitapp.com/v3";
